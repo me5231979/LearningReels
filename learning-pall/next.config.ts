83d3230a56b5cca9fa32ae7785bef34a262aeed8
Matt Estes <mattestes523@gmail.com>
2026-04-11 12:28:12 -0400

    disable PWA for production build

--- learning-pall/next.config.ts
+++ learning-pall/next.config.ts
@@ -3,14 +3,11 @@ import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  experimental: {
-    turbo: {},
-  },
 };
 
 export default withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: true,
 })(nextConfig);
