7786905c15b6b848933623aa1a811ec8e6bb08cb
Matt Estes <mattestes523@gmail.com>
2026-04-11 15:16:04 -0400

    fix prisma config - remove hardcoded neon init

--- learning-pall/prisma/prisma.config.ts
+++ learning-pall/prisma/prisma.config.ts
@@ -1,12 +1,3 @@
 import { defineConfig } from "prisma/config";
-import { neon } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 
-const sql = neon("postgresql://neondb_owner:npg_mjEH7AFb2XCo@ep-mute-cake-amsgy8nu-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
-const adapter = new PrismaNeon(sql);
-
-export default defineConfig({
-  datasource: {
-    adapter,
-  },
-});
+export default defineConfig({});
